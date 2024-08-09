b174d5560abfdfff90c5766f8653b9682230519c
Rodrigo Queiroz <rodrigogqueiroz@live.com>
2024-08-09 10:09:50 -0300

    init repo

---
+++ MenuDeCasa/MenuDeCasa/MenuDeCasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuDeCasaApp.swift
+//  MenuDeCasa
+//
+//  Created by Rodrigo Queiroz on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuDeCasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MenuDeCasa
 Projeto de estudo do SwiftUI 
+
+- Projeto idealizado para estudar componentes visuais e navegação. 
